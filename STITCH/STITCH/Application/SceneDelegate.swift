0704ba28b082cc3265f3804298c0565872d6cb61
neuli <asdfz888@naver.com>
2023-02-14 23:54:33 +0900

    feat: foldering

---
+++ STITCH/STITCH/Application/SceneDelegate.swift
@@ -0,0 +1,35 @@
+//
+//  SceneDelegate.swift
+//  STITCH
+//
+//  Created by neuli on 2023/02/14.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        start(windowScene)
+    }
+    
+    func start(_ windowScene: UIWindowScene) {
+        let navigationController = UINavigationController()
+        window = UIWindow(windowScene: windowScene)
+        
+        window?.rootViewController = navigationController
+        window?.backgroundColor = .white // TODO: color 변경
+        window?.makeKeyAndVisible()
+        
+        // TODO: appCoordinator 추가
+        // appCoordinator start
+    }
+}
+
