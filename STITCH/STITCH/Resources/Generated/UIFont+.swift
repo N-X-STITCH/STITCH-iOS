d41b9cf3ad7f788e57acd68155e9ed12c1068879
neuli <asdfz888@naver.com>
2023-02-16 18:37:58 +0900

    feat: font 추가 #3

---
+++ STITCH/STITCH/Resources/Generated/UIFont+.swift
@@ -0,0 +1,40 @@
+//
+//  UIFont+.swift
+//  STITCH
+//
+//  Created by neuli on 2023/02/16.
+//
+
+import UIKit
+
+// Pretendard-Regular
+// Pretendard-SemiBold
+// Pretendard-Bold
+
+extension UIFont {
+    // Title
+    static let Display1_28 = UIFont(name: "Pretendard-Bold", size: 28)
+    static let Display1_38 = UIFont(name: "Pretendard-Bold", size: 38)
+    static let Display2_20 = UIFont(name: "Pretendard-Bold", size: 20)
+    static let Display2_34 = UIFont(name: "Pretendard-Bold", size: 34)
+    static let Headline_20 = UIFont(name: "Pretendard-SemiBold", size: 20)
+    static let Headline_28 = UIFont(name: "Pretendard-SemiBold", size: 28)
+    static let Subhead_16 = UIFont(name: "Pretendard-SemiBold", size: 16)
+    static let Subhead_24 = UIFont(name: "Pretendard-SemiBold", size: 24)
+    
+    // Body
+    static let Body1_16 = UIFont(name: "Pretendard-Regular", size: 16)
+    static let Body1_24 = UIFont(name: "Pretendard-Regular", size: 24)
+    static let Body1_28 = UIFont(name: "Pretendard-Regular", size: 28)
+    static let Body2_14 = UIFont(name: "Pretendard-Regular", size: 14)
+    static let Body2_20 = UIFont(name: "Pretendard-Regular", size: 20)
+    static let Body2_24 = UIFont(name: "Pretendard-Regular", size: 24)
+    
+    // Caption
+    static let Caption1_12 = UIFont(name: "Pretendard-Regular", size: 12)
+    static let Caption1_18 = UIFont(name: "Pretendard-Regular", size: 18)
+    static let Caption2_10 = UIFont(name: "Pretendard-Regular", size: 10)
+    static let Caption2_16 = UIFont(name: "Pretendard-Regular", size: 16)
+    static let Caption3_12 = UIFont(name: "Pretendard-SemiBold", size: 12)
+    static let Caption3_18 = UIFont(name: "Pretendard-SemiBold", size: 18)
+}
