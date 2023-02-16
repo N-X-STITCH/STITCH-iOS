dbcb43a24b2bbab243364c403d4f0f33070a3683
neuli <asdfz888@naver.com>
2023-02-16 19:48:58 +0900

    feat: image, color 변경

---
+++ STITCH/STITCH/Resources/Generated/UIImage+.swift
@@ -0,0 +1,13 @@
+//
+//  UIImage+.swift
+//  STITCH
+//
+//  Created by neuli on 2023/02/16.
+//
+
+import UIKit
+
+extension UIImage {
+    static let appleLoginButton = UIImage(named: "appleLoginButton")
+    static let kakaoLoginButton = UIImage(named: "kakaoLoginButton")
+}
