442d45da43cd169342aef17f2430a85aefb3913d
neuli <asdfz888@naver.com>
2023-02-16 14:23:10 +0900

    feat: auth 파일 생성 #7

---
+++ STITCH/STITCH/Domain/UseCase/Auth/AuthUseCase.swift
@@ -0,0 +1,26 @@
+//
+//  AuthUseCase.swift
+//  STITCH
+//
+//  Created by neuli on 2023/02/15.
+//
+
+import Combine
+import Foundation
+
+protocol AuthUseCase {
+    
+}
+
+final class DefaultAuthUseCase: AuthUseCase {
+    
+    // MARK: - Properties
+    
+    // MARK: - Initializer
+    
+    init() {
+        
+    }
+    
+    // MARK: - Methods
+}
