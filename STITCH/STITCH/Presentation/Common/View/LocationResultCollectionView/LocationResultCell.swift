8b552a18018fcac797fc50582c19e8d7bdf616e1
neuli <asdfz888@naver.com>
2023-03-13 21:49:30 +0900

    feat: LocationCollectionViewCell LocationInfo 모델 포함으로 수정

---
+++ STITCH/STITCH/Presentation/Common/View/LocationResultCollectionView/LocationResultCell.swift
@@ -0,0 +1,12 @@
+//
+//  LocationResultCell.swift
+//  STITCH
+//
+//  Created by neuli on 2023/03/13.
+//
+
+import UIKit
+
+final class LocationResultCell: UICollectionViewListCell {
+    var location: LocationInfo!
+}
