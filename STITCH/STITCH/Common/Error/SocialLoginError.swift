b8013e39705ccbaface23cb376eec01ced5ec5f9
neuli <asdfz888@naver.com>
2023-03-14 23:06:20 +0900

    feat: 에러 localizedString 추가

--- STITCH/STITCH/Common/Error/SocialLoginError.swift
+++ STITCH/STITCH/Common/Error/SocialLoginError.swift
@@ -7,7 +7,14 @@
 
 import Foundation
 
-enum SocialLoginError: Error {
+enum SocialLoginError: LocalizedError {
     case canNotAvailableKakaoLogin
     case unknown
+    
+    var errorDescription: String {
+        switch self {
+        case .canNotAvailableKakaoLogin: return "카카오 로그인 실패"
+        case .unknown: return "알 수 없는 에러입니다."
+        }
+    }
 }
