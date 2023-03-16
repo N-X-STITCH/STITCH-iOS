2b60790055a131e9e9ad8b2195ed7b05077a0899
neuli <asdfz888@naver.com>
2023-03-16 13:28:30 +0900

    feat: LocationInfo Model 추가

--- STITCH/STITCH/Presentation/Model/LocationInfo.swift
+++ STITCH/STITCH/Presentation/Model/LocationInfo.swift
@@ -9,4 +9,16 @@ import Foundation
 
 struct LocationInfo: Hashable {
     let address: String
+    let latitude: String?
+    let longitude: String?
+    
+    init(
+        address: String,
+        latitude: String? = nil,
+        longitude: String? = nil
+    ) {
+        self.address = address
+        self.latitude = latitude
+        self.longitude = longitude
+    }
 }
