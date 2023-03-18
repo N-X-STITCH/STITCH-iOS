64898fa4db282e64dd4db63e2b7df97769a0364b
neuli <asdfz888@naver.com>
2023-03-19 01:25:55 +0900

    feat: katech -> 위도 경도 좌표계 변환

--- STITCH/STITCH/Presentation/Model/LocationInfo.swift
+++ STITCH/STITCH/Presentation/Model/LocationInfo.swift
@@ -5,20 +5,46 @@
 //  Created by neuli on 2023/03/13.
 //
 
+import CoreLocation
 import Foundation
+import MapKit
 
 struct LocationInfo: Hashable {
-    let address: String
-    let latitude: String?
-    let longitude: String?
+    var address: String
+    var roadAddress: String?
+    var latitude: String?
+    var longitude: String?
+    var katechX: String?
+    var katechY: String?
     
     init(
         address: String,
+        roadAddress: String? = nil,
         latitude: String? = nil,
-        longitude: String? = nil
+        longitude: String? = nil,
+        katechX: String? = nil,
+        katechY: String? = nil
     ) {
         self.address = address
+        self.roadAddress = roadAddress
         self.latitude = latitude
         self.longitude = longitude
+        self.katechX = katechX
+        self.katechY = katechY
+    }
+    
+    mutating func convertKatechToGEO() {
+        guard let katechX = katechX,
+              let katechX = Double(katechX),
+              let katechY = katechY,
+              let katechY = Double(katechY)
+        else { return }
+        
+        let geoTrans = GeoTrans()
+        geoTrans.set()
+        let katech = GeoTransPoint(katechX, katechY)
+        let geo = geoTrans.convert(1, 0, katech)
+        latitude = "\(geo.getY())"
+        longitude = "\(geo.getX())"
     }
 }
