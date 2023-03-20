22fb93e3a5d576faa7c107de945937afbab9fe30
neuli <asdfz888@naver.com>
2023-03-20 22:27:34 +0900

    feat: 설정 Section 변경

--- STITCH/STITCH/Presentation/TabBar/MyMenu/Setting/SettingSection.swift
+++ STITCH/STITCH/Presentation/TabBar/MyMenu/Setting/SettingSection.swift
@@ -18,6 +18,14 @@ enum SettingSection: CaseIterable {
         }
     }
     
+    var section: Int {
+        switch self {
+        case .guide: return 0
+        case .account: return 1
+        case .none: return 2
+        }
+    }
+    
     func rowTitle(_ indexPath: IndexPath) -> String {
         switch indexPath.section {
         case 0: return GuideSection.allCases[indexPath.row].title
@@ -29,12 +37,20 @@ enum SettingSection: CaseIterable {
 }
 
 enum GuideSection: CaseIterable {
-    case help, opinion
+//    case help
+    case opinion
     
     var title: String {
         switch self {
-        case .help: return "고객센터/도움말"
+//        case .help: return "고객센터/도움말"
         case .opinion: return "개선 및 의견 남기기"
+        }
+    }
+    
+    var row: Int {
+        switch self {
+//        case .help: return 0
+        case .opinion: return 0
         }
     }
 }
@@ -48,17 +64,31 @@ enum AccountSection: CaseIterable {
         case .secession: return "계정탈퇴"
         }
     }
+    
+    var row: Int {
+        switch self {
+        case .logout: return 0
+        case .secession: return 1
+        }
+    }
 }
 
 enum NoneSection: CaseIterable {
     case version, terms, policy
-    
     
     var title: String {
         switch self {
         case .version: return "현재 버전"
         case .terms: return "서비스 이용 약관"
         case .policy: return "개인정보 처리방침"
+        }
+    }
+    
+    var row: Int {
+        switch self {
+        case .version: return 0
+        case .terms: return 1
+        case .policy: return 2
         }
     }
 }
