095ecc7c3365e1262684067295431e1cbe3146ac
neuli <asdfz888@naver.com>
2023-03-20 17:07:24 +0900

    feat: 스크롤뷰 높이 구하는 형식 변경

--- STITCH/STITCH/Common/Extension/UIScrollView.swift
+++ STITCH/STITCH/Common/Extension/UIScrollView.swift
@@ -8,12 +8,6 @@
 import UIKit
 
 extension UIScrollView {
-    func updateContentSize() {
-        let unionCalculatedTotalRect = recursiveUnionInDepthFor(view: self)
-
-        self.contentSize = CGSize(width: self.frame.width, height: unionCalculatedTotalRect.height+50)
-    }
-    
     func recursiveUnionInDepthFor(view: UIView) -> CGRect {
         var totalRect: CGRect = .zero
         
